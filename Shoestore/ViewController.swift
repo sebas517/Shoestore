882bfdbc4969626398607e187e2c543b3cb65469
Sebastián Pérez Moreno <sebastianperez517@gmail.com>
2019-02-04 13:24:59 +0100

    Initial Commit

---
+++ Shoestore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shoestore
+//
+//  Created by dam on 4/2/19.
+//  Copyright © 2019 dam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
