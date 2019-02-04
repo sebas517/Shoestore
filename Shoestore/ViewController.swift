bb60f5b228d92f773b1d3f001da5647c4e448d59
Sebastián Pérez Moreno <sebastianperez517@gmail.com>
2019-02-04 14:28:51 +0100

    prueba4

--- Shoestore/ViewController.swift
+++ Shoestore/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("Este es un proyecto prueba3")
+        print("Este es un proyecto prueba4")
     }
 
 
