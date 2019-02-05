d50a8f23aa4e54f22ace28d38399bcc6feb4435a
neaker01 <snakeone93@hotmail.com>
2019-02-05 09:23:51 +0100

    prueba8

--- Shoestore/ViewController.swift
+++ Shoestore/ViewController.swift
@@ -14,6 +14,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("Este es un proyecto prueba 7")
+        
+        
+        print("HOLA")
+        
+        
+        
     }
 
 
