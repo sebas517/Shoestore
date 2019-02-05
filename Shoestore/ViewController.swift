643e6ecc26bf2b00d0cc34b9dae8fe413aa585fb
Sebastián Pérez Moreno <sebastianperez517@gmail.com>
2019-02-05 08:57:45 +0100

    prueba fin

--- Shoestore/ViewController.swift
+++ Shoestore/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("Este es un proyecto prueba 23333")
+        print("Este es un proyecto orueba fin")
     }
 
 
