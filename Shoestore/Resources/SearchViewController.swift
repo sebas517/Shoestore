57ddef72f6b5c454a2617e44e1e36f6fc4ad465e
dam <dam@pc34-12.18700098.18.andared.ced.junta-andalucia.es>
2019-02-07 09:27:44 +0100

    search bar funcional

--- Shoestore/Resources/SearchViewController.swift
+++ Shoestore/Resources/SearchViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    let testArray = ["Spain", "Canada", "Venezuela", "Suiza", "Italia", "Eslovenia", "China", "Australia", "Marruecos", "India"];
+    
+    var searchItem = [String]()
+    var searching = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -25,5 +33,38 @@ class SearchViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+}
 
+extension SearchViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if searching {
+            return searchItem.count
+        } else {
+            return testArray.count
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        if searching {
+            cell?.textLabel?.text = searchItem[indexPath.row]
+        } else {
+            cell?.textLabel?.text = testArray[indexPath.row]
+        }
+        return cell!
+    }
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String){
+        searchItem = testArray.filter({$0.lowercased().prefix(searchText.count) == searchText.lowercased()})
+        searching = true
+        tableView.reloadData()
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searching = false
+        searchBar.text = ""
+        tableView.reloadData()
+    }
 }
