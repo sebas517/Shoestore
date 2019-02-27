ab157cb5226a915bb53f415824b2d5a4892c0467
dam <dam@imac-de-izv-5.local>
2019-02-27 13:50:34 +0100

    account settings

--- Shoestore/Resources/AccountSettings/AccountSettingsController.swift
+++ Shoestore/Resources/AccountSettings/AccountSettingsController.swift
@@ -9,7 +9,7 @@
 import Foundation
 import UIKit
 
-class AccountSettingsController: UIViewController, UIImagePickerControllerDelegate, OnResponse {
+class AccountSettingsController: UIViewController, UIImagePickerControllerDelegate{
     var usuario:User?
     let preferences = UserDefaults.standard
     
@@ -36,72 +36,29 @@ class AccountSettingsController: UIViewController, UIImagePickerControllerDelega
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        /*imagePicker.delegate = (self as! UIImagePickerControllerDelegate & UINavigationControllerDelegate)*/
         loadUsers()
         loadData()
     }
     
-    func onData(data: Data) {
-        do {
-            let decoder = JSONDecoder()
-            let usuarios = try decoder.decode(Usuario.self, from:data)
-            
-            
-            if usuarios.usuario.count < 1 {
-                print("Usuario o clave incorrectos")
-            } else {
-                //print("\(usuarios.usuario[0])")
-                for usuarioRest in usuarios.usuario{
-                    print("foreach---\(usuarioRest.login)...\(usuarioRest.clave)" )
-                    usuario = User(id: Int(usuarioRest.id) ?? 0, login: usuarioRest.login, key: usuarioRest.clave, email: usuarioRest.correo, name: usuarioRest.nombre, lastname: usuarioRest.apellidos, address: usuarioRest.direccion, signedUp: stringToDate(usuarioRest.fecha_alta), active: Bool(usuarioRest.activo) ?? false, admin: Bool(usuarioRest.activo) ?? false)
-                }
-        
-            }
-        } catch let parsingError {
-            print("Error", parsingError)
-        }
-    }
-    
-    func onDataError(message: String) {
-        print("onError")
-    }
-    
     func loadUsers() {
-        let userId = UserDefaults.standard.integer(forKey: "userId")
-        
-        guard let cliente = RestClient(service: "usuario/\(userId)",response: self) else {
+        guard let userData = UserDefaults.standard.object(forKey: "userData") as? NSData else {
+            print("'shopBag' not found in UserDefaults")
             return
         }
         
-        print("ID:\(userId)")
-        cliente.request()
-        
-        /*
-        guard let usuarios = NSKeyedUnarchiver.unarchiveObject(with: users as Data) as? [User] else{
+        guard let user = NSKeyedUnarchiver.unarchiveObject(with: userData as Data) as? User else {
             print("Could not unarchive from placesData")
             return
         }
-    
-        if(usuarios.count > 0) {
-            self.usuarios = usuarios
-        }*/
-    }
-    
-    public func stringToDate(_ strings: String) -> Date {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy-MM-dd"
-        formatter.locale = Locale(identifier: "en_US_POSIX")
-        let date = formatter.date(from:strings)!
-        let calendar = Calendar.current
-        let components = calendar.dateComponents([.year, .month, .day, .hour], from: date)
-        let finalDate = calendar.date(from:components)
-        return finalDate!
+        
+        usuario = user
+        
     }
     
     func loadData() {
-        nameTf.text = usuario?.name/* ?? "Usuario"*/
-        surnameTf.text = usuario?.lastname/* ?? "Clave"*/
-        adressTf.text = usuario?.address/* ?? "Direccion"*/
-        emailTf.text = usuario?.email/* ?? "E-mail"*/
+        nameTf.text = usuario?.getName()/* ?? "Usuario"*/
+        surnameTf.text = usuario?.getLastname()/* ?? "Clave"*/
+        adressTf.text = usuario?.getAddress()/* ?? "Direccion"*/
+        emailTf.text = usuario?.getEmail()/* ?? "E-mail"*/
     }
 }
